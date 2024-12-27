4e5278215ca55a49d50e5c318fa550cb2241f8c5
Alex Meckes <alex.meckes@gmail.com>
2024-12-26 20:47:00 -0500

    Fix router setup and move BrowserRouter to root level

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider';
 import CreateGroup from './components/CreateGroup';
 import GroupDashboard from './components/GroupDashboard';
@@ -8,18 +8,16 @@ import BingoCard from './components/BingoCard';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<CreateGroup />} />
-          <Route path="/join/:groupId" element={<JoinGroup />} />
-          <Route path="/group/:groupId" element={<GroupDashboard />} />
-          <Route path="/group/:groupId/submit" element={<SubmitPrediction />} />
-          <Route path="/group/:groupId/card" element={<BingoCard />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<CreateGroup />} />
+        <Route path="/join/:groupId" element={<JoinGroup />} />
+        <Route path="/group/:groupId" element={<GroupDashboard />} />
+        <Route path="/group/:groupId/submit" element={<SubmitPrediction />} />
+        <Route path="/group/:groupId/card" element={<BingoCard />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
